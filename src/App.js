1647399ae28ae5e1c3c1678f02347f62a827eed6
Muhammad Sufyan <51535061+SamiSahai@users.noreply.github.com>
2020-06-24 21:23:15 -0800

    installed fafilePdf
    Installed Pdf library and render Pdf function also added library to global provider

--- src/App.js
+++ src/App.js
@@ -13,9 +13,15 @@ import { AddTransaction } from './components/AddTransaction';
 // Import PdfDocument
 import {PDFDownloadLink} from "@react-pdf/renderer";
 import { PdfDocument } from "./components/PdfDocument/pdfDocument";
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 // Import Provider
 import { GlobalProvider } from './context/GlobalState';
+
+
+library.add(faFilePdf);
 
 function App() {
   return (
